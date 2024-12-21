920df7be3fa5159348e6e62f234bc37bc567efe8
Sahasra Ranjan <sahasraranjan@gmail.com>
2024-12-21 23:51:46 +0530

    v1.0.5

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,33 @@
-declare module 'lior-game' {
-  export function SandboxProvider(props: any): React.ReactElement;
-  export function useSandboxContext(): {
-    sendAdminMessage: (role: string, content: string) => void;
-  };
-  export function LiorGameProvider(props: any): React.ReactElement;
-  export function useLiorGame(): any;
-}
+import React, { ReactNode } from 'react';
+import { SandboxProvider as SandboxProviderComponent, useSandboxContext as useSandboxContextHook } from './components/sandbox';
+import { LiorGameProvider as LiorGameProviderComponent, useLiorGame as useLiorGameHook } from './components/lior-game-provider';
+import * as CustomUI from './components/custom_ui/index';
+import SuccessAnimate from './components/utils/success-animate';
+
+export const SandboxProvider: React.FC<{ children: ReactNode }> = SandboxProviderComponent;
+export const useSandboxContext: () => {
+  componentRef: React.RefObject<HTMLDivElement> | null;
+  sendAdminMessage?: (role: string, content: string) => Promise<void>;
+} = useSandboxContextHook;
+
+export const LiorGameProvider: React.FC<{ children: ReactNode, wsUrl?: string }> = LiorGameProviderComponent;
+export const useLiorGame: () => any = useLiorGameHook;
+
+export const { 
+  Button, 
+  Card, 
+  Slider, 
+  Switch, 
+  Select, 
+  Accordion, 
+  DropdownMenu, 
+  ScrollArea, 
+  Toggle, 
+  ToggleGroup, 
+  Progress, 
+  Label 
+} = CustomUI;
+
+export const SuccessAnimation = SuccessAnimate;
+
+export const CustomComponents = CustomUI;
