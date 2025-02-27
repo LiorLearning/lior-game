815deb42f162076a39a7ea84220452ae89ffe4ce
Sahasra Ranjan <sahasraranjan@gmail.com>
2025-02-27 16:23:29 +0530

    Update newinput

--- src/components/custom_ui/newinput.tsx
+++ src/components/custom_ui/newinput.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 
 export interface NewInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   value: string;
